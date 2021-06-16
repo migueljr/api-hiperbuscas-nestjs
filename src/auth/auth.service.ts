5d09961b65275c680785023220adcc9861f9b4d3
Miguel Jr <miguel_jr@mail.com>
2021-06-16 00:34:28 -0300

    return senha in payload token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,6 +24,8 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      name: user.name,
+      email: user.email
     };
   }
 }
