e0547d4e6622d3568a0a02c02fc6be26e4de11ac
Miguel Jr <miguel_jr@mail.com>
2021-06-17 07:12:44 -0300

    deploy

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://hiperbuscas.com/nest', {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true}),
+      mongoose.connect('mongodb://localhost/nest', {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true}),
   },
 ];
